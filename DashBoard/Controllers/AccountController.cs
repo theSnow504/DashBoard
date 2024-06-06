74c6a1844e823f42b702b7069a73a3488657fa01
Trung <quangtrung0965@gmail.com>
2024-06-06 15:48:41 +0700

    view for login, forgot password

---
+++ DashBoard/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DashBoard.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            return View();
+        }
+
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ForgotPassword(string username, string license)
+        {
+            return View();
+        }
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+    }
+}
