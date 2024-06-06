8d73d2fd7a053f605591940c0c90fe13eb2fd637
Admin <Admin@DESKTOP-48JPN0S>
2024-06-06 16:28:54 +0700

    aa

---
+++ DashBoard/Models/District.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DashBoard.Models
+{
+    public partial class District
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? KeyWord { get; set; }
+        public int IdProvince { get; set; }
+        public int? OrderNum { get; set; }
+
+        public virtual Province IdProvinceNavigation { get; set; } = null!;
+    }
+}
---
+++ DashBoard/Models/GroupFb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DashBoard.Models
+{
+    public partial class GroupFb
+    {
+        public GroupFb()
+        {
+            Posts = new HashSet<Post>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Url { get; set; } = null!;
+        public int IdFaceBook { get; set; }
+        public string? Type { get; set; }
+        public int? NumMember { get; set; }
+        public int? NumPostPerDay { get; set; }
+        public string? Description { get; set; }
+        public DateTime? DateUpdate { get; set; }
+
+        public virtual AccountFb IdFaceBookNavigation { get; set; } = null!;
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
