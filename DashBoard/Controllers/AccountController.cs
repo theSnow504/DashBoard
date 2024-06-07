75d2e914012b9e30113d108c65d257955c66a2e3
Trung <quangtrung0965@gmail.com>
2024-06-07 15:16:06 +0700

    fix conflict

--- DashBoard/Controllers/AccountController.cs
+++ DashBoard/Controllers/AccountController.cs
@@ -1,34 +1,14 @@
 ﻿using Dashboard.DataDto.User;
-using DashBoard.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DashBoard.Controllers
 {
     public class AccountController : Controller
-    {
-        private readonly IAccountService _service;
-        public AccountController(IAccountService service)
-        {
-            _service = service;
-        }
-
-
+    {    
         [HttpGet]
         public ActionResult Login()
         {
-            string? userId = Request.Cookies["userId"];
-            if (userId == null) 
-            {
-                return View();
-            }
-            ResponseBase<User?> response = _service.Login(userId);
-            if (response.Data == null)
-            {
-                ViewData["error"] = response.Message;
-                return View("/Views/Shared/Error.cshtml");
-            }
-            HttpContext.Session.SetInt32("userId", response.Data.Id);
-            return Redirect("/Home/Index");
+            return View();
         }
 
         [HttpPost]
@@ -40,9 +20,7 @@ namespace DashBoard.Controllers
 
         public IActionResult Logout()
         {
-            HttpContext.Session.Clear();
-            HttpContext.Session.Remove("User");
-            return RedirectToAction("Login", "Account");
+             return View();
         }
 
         [HttpGet]
@@ -58,20 +36,9 @@ namespace DashBoard.Controllers
         }
 
 
-        public IActionResult ChangePassword()
-            ResponseBase<User?> response = _service.ForgotPassword(username, license);
-            if(response.Data == null)
-            {
-                if (response.Code == (int)HttpStatusCode.NotFound)
-                {
-                    ViewData["error"] = response.Message;
-                    return View();
-                }
-                ViewData["error"] = response.Message;
-                return View("/Views/Shared/Error.cshtml");
-            }
-           
-            return Redirect("/Account/ResetPassword");
+        public IActionResult ChangePassword() 
+        { 
+            return View(); 
         }
 
         public ActionResult ResetPassword()
